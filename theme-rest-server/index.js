e83f315fce01ffb4c81a81739b46c2b9fa961042
Krum Bezmenov <krum.bezmenov@gmail.com>
2024-08-01 14:15:24 +0300

    React-router implementation

--- theme-rest-server/index.js
+++ theme-rest-server/index.js
@@ -17,7 +17,7 @@ app.use(authMiddleware);
 app.use(routes);
 
 mongoose
-  .connect(`mongodb://127.0.0.1:27017/books-forum-db`)
+  .connect(`mongodb://127.0.0.1:27017/themes-app-db`)
   .then(() => {
     console.log(`DB Connected`);
     app.listen(port, () =>
