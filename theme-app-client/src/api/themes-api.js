0e5389daad0ef950fc4085d54cc11eb9f87ee228
Krum Bezmenov <krum.bezmenov@gmail.com>
2024-08-07 12:32:53 +0300

    Themes functions update

--- theme-app-client/src/api/themes-api.js
+++ theme-app-client/src/api/themes-api.js
@@ -5,7 +5,23 @@ const THEMES_URL = environment.themesUrl;
 
 export const getAll = async () => {
   const result = await request.get(THEMES_URL);
-
   const themes = Object.values(result);
   return themes;
 };
+
+export const searchThemes = async (query) => {
+  const result = await request.get(`${THEMES_URL}/search?q=${query}`);
+  const themes = Object.values(result);
+  return themes;
+};
+
+export const getOne = (themeId) =>
+  request.get(`${THEMES_URL}/${themeId}/details`);
+
+const themesAPI = {
+  getAll,
+  getOne,
+  searchThemes,
+};
+
+export default themesAPI;
