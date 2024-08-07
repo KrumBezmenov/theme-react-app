5c06d7f4733f2b5e47401d5cfeadec4bd1aa9d2b
Krum Bezmenov <krum.bezmenov@gmail.com>
2024-08-07 12:36:46 +0300

    Updated Register

--- theme-app-client/src/components/user/Register.jsx
+++ theme-app-client/src/components/user/Register.jsx
@@ -1,99 +1,158 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function RegisterCard() {
-  const url = "https://jsonplaceholder.typicode.com/users";
-  const [data, setData] = useState([]);
+async function registerUser(credentials) {
+  return fetch("http://localhost:4000/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
 
-  const fetchInfo = () => {
-    return fetch(url)
-      .then((res) => res.json())
-      .then((d) => setData(d));
+function RegisterCard({ setToken }) {
+  function validateForm() {
+    if (email.length == 0) {
+      alert("Invalid Form, Email Address can not be empty");
+      return;
+    }
+
+    if (email.length <= 10) {
+      alert("Invalid Form, Email Address can not be less than 10 characters");
+
+      return;
+    }
+
+    if (password.length < 4) {
+      alert(
+        "Invalid Form, Password must contain greater than or equal to 4 characters."
+      );
+      return;
+    }
+
+    if (password.length == 0) {
+      alert("Invalid Form, Password must can not be empty.");
+      return;
+    }
+
+    if (password !== rePassword) {
+      alert("Password missmatch");
+      return;
+    }
+
+    alert("Welcome back");
+  }
+  const navigate = useNavigate();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [rePassword, setRePassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password !== rePassword) {
+      return;
+    }
+
+    const token = await registerUser({
+      email,
+      password,
+      rePassword,
+    });
+    setToken(token);
+    navigate("/");
   };
-
-  useEffect(() => {
-    fetchInfo();
-  }, []);
-
   return (
-    <div class="selection:bg-rose-500 selection:text-white">
-      <div class="min-h-screen bg-purple-200 flex justify-center items-center">
-        <div class="p-8 flex-1">
-          <div class="w-80 bg-white rounded-3xl mx-auto overflow-hidden shadow-xl">
-            <div class="relative h-48 bg-yellow-500 rounded-bl-4xl">
+    <div className="selection:bg-rose-500 selection:text-white">
+      <div className="min-h-screen bg-purple-200 flex justify-center items-center">
+        <div className="p-8 flex-1">
+          <div className="w-80 bg-white rounded-3xl mx-auto overflow-hidden shadow-xl">
+            <div className="relative h-48 bg-yellow-500 rounded-bl-4xl">
               <svg
-                class="absolute bottom-0"
+                className="absolute bottom-0"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 1440 320"
               >
                 <path
                   fill="#ffffff"
-                  fill-opacity="1"
+                  fillOpacity="1"
                   d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,85.3C672,75,768,85,864,122.7C960,160,1056,224,1152,245.3C1248,267,1344,245,1392,234.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
                 ></path>
               </svg>
             </div>
-            <div class="px-10 pt-4 pb-8 bg-white rounded-tr-4xl">
-              <h1 class="text-2xl font-semibold text-gray-900">Join now!</h1>
-              <form class="mt-12" action="" method="POST">
-                <div class="relative">
+            <div className="px-10 pt-4 pb-8 bg-white rounded-tr-4xl">
+              <h1 className="text-2xl font-semibold text-gray-900">
+                Join now!
+              </h1>
+              <form className="mt-12" method="POST" onSubmit={handleSubmit}>
+                <div className="relative">
                   <input
                     id="email"
                     name="email"
                     type="text"
-                    class="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-yellow-600"
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-yellow-600"
                     placeholder="john@doe.com"
                   />
                   <label
-                    for="email"
-                    class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    htmlFor="email"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
                   >
                     Email address
                   </label>
                 </div>
 
-                <div class="mt-10 relative">
+                <div className="mt-10 relative">
                   <input
                     id="password"
                     type="password"
                     name="password"
-                    class="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-yellow-600"
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-yellow-600"
                     placeholder="Password"
                   />
                   <label
-                    for="password"
-                    class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    htmlFor="password"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
                   >
                     Password
                   </label>
                 </div>
-                <div class="mt-10 relative">
+                <div className="mt-10 relative">
                   <input
-                    id="password"
+                    id="rePassword"
                     type="password"
-                    name="password"
-                    class="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-yellow-600"
-                    placeholder="Password"
+                    name="rePassword"
+                    onChange={(e) => setRePassword(e.target.value)}
+                    className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-yellow-600"
+                    placeholder="rePassword"
                   />
                   <label
-                    for="password"
-                    class="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    htmlFor="rePassword"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
                   >
                     Confirm Password
                   </label>
                 </div>
 
-                <div class="mt-20 px-4 py-2 rounded bg-yellow-500 hover:bg-yellow-400 text-white font-semibold text-center block w-full focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-500 focus:ring-opacity-80 cursor-pointer">
-                  <Link to="/home">
-                    <button class=" text-white py-2 px-4 rounded-full font-bold ">
-                      Register
-                    </button>
-                  </Link>
+                <div className="btn-submit mt-20 px-4 py-2 rounded bg-yellow-500 hover:bg-yellow-400 text-white font-semibold text-center block w-full focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-500 focus:ring-opacity-80 cursor-pointer">
+                  <button className=" text-white py-2 px-4 rounded-full font-bold ">
+                    <input
+                      onClick={() => {
+                        validateForm();
+                      }}
+                      value="Register"
+                      type="submit"
+                      className="btn-submit"
+                    ></input>
+                  </button>
                 </div>
               </form>
               <Link
                 to="/login"
-                class="mt-4 block text-sm text-center font-medium text-yellow-600 hover:underline focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                className="mt-4 block text-sm text-center font-medium text-yellow-600 hover:underline focus:outline-none focus:ring-2 focus:ring-yellow-500"
               >
                 {" "}
                 Already have an account?{" "}
@@ -107,3 +166,6 @@ function RegisterCard() {
 }
 
 export default RegisterCard;
+RegisterCard.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
